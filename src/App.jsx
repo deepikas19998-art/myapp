be39e8c62a28d9a3dcf23d5c637559a37aedf4a7
deep <your_github_deepikas19998@gmail.com>
2025-12-09 23:08:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+const [ToDoList, setToDoList] = useState([]);
+
+  const saveToDoList = (event) => {
+    event.preventDefault();
+
+    const toName = event.target.toName.value.trim();
+
+    if (!ToDoList.includes(toName)) {
+      const finalToDoList = [...ToDoList, toName]
+      setToDoList([...ToDoList, toName]);
+    } else {
+      alert("ToDo Name Already Exists...");
+    }
+  };
+
+  const list = ToDoList.map((value, index)=> {
+    return (
+<ToDoListItems value={value} key={index} indexNumber={index} 
+ToDoList={ToDoList}
+setToDoList={setToDoList}
+/>
+    )
+  })
+
+
+  return (
+       
+      <div className='outerDiv'>
+        <h1>Todo List</h1>
+        <form onSubmit={saveToDoList}>
+          <input type= "text" name = 'toName' className="rgb-input"/><button class="save">
+            <span>Save</span>
+          </button>
+        </form>
+        <div>
+        <ul>
+          {list}
+        </ul>
+        </div>
+        </div>
+  )
+}
+
+export default App
+
+function ToDoListItems({value, indexNumber, ToDoList, setToDoList}){
+  const [status, setStatus] = useState(false)
+  const deleteRow = () => {
+    const finalData = ToDoList.filter((v,i)=> i!= indexNumber)
+    setToDoList(finalData)
+  }
+
+  const checkStatus = () => {
+  setStatus(!status)
+  }
+  return(
+    <li className={(status)? 'completeToDo': ''} onClick={checkStatus}> {indexNumber+1} {value} <span onClick={deleteRow}>&times;</span></li>
+  )
+}
